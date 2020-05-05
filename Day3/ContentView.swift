0e6a64aa5a7deedb786468b08f9e4bf91836331e
Bader Alsabah <Alsabah.dev@gmail.com>
2020-05-05 21:45:39 +0300

    Initial Commit

---
+++ Day3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day3
+//
+//  Created by Bader Alsabah on 5/5/20.
+//  Copyright © 2020 Bader Alsabah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
